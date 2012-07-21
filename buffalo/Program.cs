ab5384bd9a8cc7e3b2e316936a65534f81d567c4
Wei Liao <wliao008@gmail.com>
2012-07-21 15:35:02 -0400

    Implementing the weaver
    Getting all methodinfo[] based on how the aspect is applied.

--- buffalo/Program.cs
+++ buffalo/Program.cs
@@ -10,56 +10,10 @@ namespace Buffalo
     {
         static void Main(string[] args)
         {
-            new Ass();
+            string path = @"C:\Users\wliao\Documents\Visual Studio 2010\Projects\buffalo\client\bin\Debug\client.exe";
+            new Weaver(path).GetAllMethods();
             Console.WriteLine("DONE");
             Console.Read();
-        }
-    }
-
-    class Ass
-    {
-        string path = @"C:\Users\wliao\Documents\Visual Studio 2010\Projects\buffalo\client\bin\Debug\client.exe";
-        public Ass()
-        {
-            Assembly assembly = Assembly.LoadFrom(path);
-            var asses = assembly.GetCustomAttributes(typeof(Aspect), false);
-            Console.WriteLine("\n\nASS-------------------");
-            foreach (var ass in asses)
-            {
-                Console.WriteLine(ass.ToString());
-            }
-
-            var types = assembly.GetTypes().ToList();
-            Console.WriteLine("\n\nTYPES-------------------");
-            foreach (var t in types)
-            {
-                Console.WriteLine(t.Name);
-                var cs = t.GetCustomAttributes(typeof(Aspect), false);
-                foreach (var c in cs)
-                {
-                    Console.WriteLine("\t" + c.ToString());
-                }
-            }
-
-            var methods = types
-                          .SelectMany(t => t.GetMethods())
-                          .Where(m => m.GetCustomAttributes(typeof(Aspect), false).Length > 0)
-                          .ToArray();
-
-            Console.WriteLine("\n\nMETHODS-------------------");
-            foreach (var m in methods)
-            {
-                Console.WriteLine(m.Name);
-                var attrs = m.GetCustomAttributesData();
-                foreach (var a in attrs)
-                {
-                    Console.WriteLine("\t" + a.ToString());
-                    foreach (var arg in a.NamedArguments)
-                    {
-                        Console.WriteLine("\t" + arg.MemberInfo.Name + ", " + arg.TypedValue);
-                    }
-                }
-            }
         }
     }
 }
--- buffalo/Weaver.cs
+++ buffalo/Weaver.cs
@@ -1,6 +1,62 @@
-﻿namespace Buffalo
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Buffalo
 {
     public class Weaver
     {
+        private string assemblyPath;
+
+        public Weaver(string assemblyPath)
+        {
+            ///TODO: check for file existant
+            this.assemblyPath = assemblyPath;
+        }
+
+        public MethodInfo[] GetAllMethods()
+        {
+            Assembly assembly = Assembly.LoadFrom(this.assemblyPath);
+            var namespaces = assembly.GetCustomAttributes(typeof(Aspect), false);
+            Console.WriteLine("NAMESPACES--------------------: " + namespaces.Count());
+            foreach (var n in namespaces)
+            {
+                Console.WriteLine(n.ToString());
+            }
+
+            var types = assembly.GetTypes().ToList();
+            Console.WriteLine("\n\nTYPES-------------------: " + types.Count);
+            foreach (var t in types)
+            {
+                Console.WriteLine(t.Name);
+                var cs = t.GetCustomAttributes(typeof(Aspect), false);
+                foreach (var c in cs)
+                {
+                    Console.WriteLine("\t" + c.ToString());
+                }
+            }
+
+            var methods = types
+                          .SelectMany(t => t.GetMethods())
+                          .Where(m => m.GetCustomAttributes(typeof(Aspect), false).Length > 0)
+                          .ToArray();
+
+            Console.WriteLine("\n\nMETHODS-------------------: " + methods.Count());
+            foreach (var m in methods)
+            {
+                Console.WriteLine(m.Name);
+                var attrs = m.GetCustomAttributesData();
+                foreach (var a in attrs)
+                {
+                    Console.WriteLine("\t" + a.ToString());
+                    foreach (var arg in a.NamedArguments)
+                    {
+                        Console.WriteLine("\t" + arg.MemberInfo.Name + ", " + arg.TypedValue);
+                    }
+                }
+            }
+
+            return null;
+        }
     }
 }
