ec6ba3c528a81f3c56497631b61d840823d754dd
Wei Liao <wliao008@gmail.com>
2012-10-24 10:54:23 -0400

    Ranamed TcfMarker -> BeginEndMarker

---
+++ buffalo/BeginEndMarker.cs
@@ -0,0 +1,10 @@
+﻿using Mono.Cecil.Cil;
+
+namespace Buffalo
+{
+    internal struct BeginEndMarker
+    {
+        public int BeginIndex { get; set; }
+        public int EndIndex { get; set; }
+    }
+}
